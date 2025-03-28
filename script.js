e69018915ebe2473bbf26003cd050a4044ec3681
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-03-28 14:25:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+
+let isLoggedIn = false;
+
+document.getElementById("submit").addEventListener("click", (e)=>{
+	 e.preventDefault();
+    let usernameInput = document.getElementById("username").value;
+    let passwordInput = document.getElementById("password").value;
+    let checkboxInput = document.getElementById("checkbox").checked;
+	let submitBtn = document.getElementById("submit");
+	let existingBtn = document.getElementById("existing");
+
+    alert(`Logged in as ${usernameInput}`);
+
+    if (checkboxInput) {
+        let userObj = {
+            username: usernameInput,
+            password: passwordInput,
+            isLoggedIn: true
+        };
+        localStorage.setItem("user", JSON.stringify(userObj));
+		submitBtn.style.display = "none";
+		existingBtn.style.display = "block";
+    } else {
+        localStorage.removeItem("user");
+    }
+})
+
+
+
+
